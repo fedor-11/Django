1db024f925ccd8db2f5a95fdc5ffbc0c7d1979a8
fedor11 <fedor.a.doronin@gmail.com>
2020-06-19 11:45:21 +0300

    делаю

--- samplesite/bboard/admin.py
+++ samplesite/bboard/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import Bd
 
-# Register your models here.
+
+admin.site.register(Bd)
+
--- samplesite/bboard/models.py
+++ samplesite/bboard/models.py
@@ -2,10 +2,16 @@ from django.db import models
 
 
 class Bd(models.Model):
-	title = models.CharField(max_length=50)
-	content = models.TextField(null=True, blank=True)
-	price = models.FloatField(null=True, blank=True)
-	published = models.DateTimeField(auto_now_add=True, db_index=True)
+	title = models.CharField(max_length=50, verbose_name="Заголовок")
+	content = models.TextField(null=True, blank=True, verbose_name="Тело статьи")
+	price = models.FloatField(null=True, blank=True, verbose_name="Цена")
+	published = models.DateTimeField(auto_now_add=True, db_index=True, verbose_name='Дата публикации')
+
+
+	class Meta:
+		verbose_name_plural = 'Объявления'
+		verbose_name = 'Объявление'
+		ordering = ['-published']
 
 
 
