6f1c39cca6117807de70f7dffd75ec792daf8def
fedor11 <fedor.a.doronin@gmail.com>
2020-06-19 14:04:07 +0300

    finished adminka

--- samplesite/bboard/admin.py
+++ samplesite/bboard/admin.py
@@ -1,6 +1,10 @@
 from django.contrib import admin
 from .models import Bd
 
+class BdAdmin(admin.ModelAdmin):
+    list_display = ('title', 'content', 'price', 'published')
+    list_display_links = ('title', 'content')
+    search_fields = ('title', 'content', )
 
-admin.site.register(Bd)
+admin.site.register(Bd, BdAdmin)
 
